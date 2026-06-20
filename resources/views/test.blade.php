0ca43bea852df2caf19af571781ec53c6270a7fe
fathur-md <fathurmuh@outlook.com>
2026-06-20 14:37:23 +0700

    feat: add initial layout and test view with team member details

---
+++ resources/views/test.blade.php
@@ -0,0 +1,44 @@
+<x-layout>
+  <section>
+    <div class="mx-auto max-w-7xl p-4">
+      <h1 class="rounded-xl bg-white px-6 py-4 text-xl font-bold text-gray-700">TIM 404 - ASN Laravel</h1>
+      <br>
+      <div class="shadow-xs relative overflow-x-auto rounded-xl border border-neutral-200 bg-neutral-50">
+        <table class="w-full text-left text-sm rtl:text-right">
+          <thead class="rounded-xl border-b border-neutral-200 bg-neutral-100 text-sm">
+            <tr>
+              <th class="px-6 py-3 font-semibold">Nama</th>
+              <th class="px-6 py-3 font-semibold">NIM</th>
+              <th class="px-6 py-3 font-semibold">Role</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            @foreach ($tim as $anggota)
+              <tr class="border-b border-neutral-200 bg-neutral-50">
+                <td class="px-6 py-4 font-semibold">
+                  {{ $anggota['nama'] }}
+                </td>
+                <td class="px-6 py-4 font-medium">
+                  {{ $anggota['nim'] }}
+                </td>
+                <td class="px-6 py-4 font-medium">
+                  {{ ucfirst($anggota['role']) }}
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+      <br>
+      {{-- Testing & Belajar Disini --}}
+      <div class="shadow-xs rounded-xl border border-neutral-200 bg-neutral-50 p-4">
+        <h2 class="font-semibold">Playground</h2>
+        <br>
+        <div>
+          <p>Test fathur</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Halo Laravel';
+    $tim = [
+        [
+            'nama'  => 'Ferdiyansyah Pratama Putra',
+            'nim'   => '241110117',
+            'role'  => 'ketua'
+        ],
+        [
+            'nama'  => 'Muhammad Fathurrahman',
+            'nim'   => '241110109',
+            'role'  => 'anggota'
+        ],
+        [
+            'nama'  => 'Dwi Angga Hermawan',
+            'nim'   => '123',
+            'role'  => 'anggota'
+        ],
+        [
+            'nama'  => 'Violetta Wungubelen',
+            'nim'   => '123',
+            'role'  => 'anggota'
+        ]
+    ];
+    return view('test', ['tim' => $tim]);
 });
