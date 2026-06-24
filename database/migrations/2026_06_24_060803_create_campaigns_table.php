5292cc4678259770348488cf8c2495148a3cbd39
ferdiyansyah pratama putra <ferdiyansyahpp@gmail.com>
2026-06-24 13:20:50 +0700

    edit table campaigns

--- database/migrations/2026_06_24_060803_create_campaigns_table.php
+++ database/migrations/2026_06_24_060803_create_campaigns_table.php
@@ -13,6 +13,23 @@ return new class extends Migration
     {
         Schema::create('campaigns', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
+            $table->string('title');
+            $table->text('description');
+            $table->string('cover_image')->nullable();
+
+            $table->decimal('target_amount', 14, 2);
+            $table->decimal('current_amount', 14, 2)->default(0);
+
+            $table->integer('donor_count')->default(0);
+
+            $table->string('category');
+            $table->enum('status', ['draft', 'active', 'completed', 'cancelled'])->default('active');
+
+            $table->timestamp('deadline_at')->nullable();
+
             $table->timestamps();
         });
     }
